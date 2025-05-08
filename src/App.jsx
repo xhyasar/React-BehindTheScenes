bb12392d5007192bd0304066916d616cfe3ba66c
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-05-08 20:37:32 +0300

    Refactor Counter to track history of changes.
    Replaced single counter state with a history-based state to track all changes. Updated components to display the current counter value derived from the history and render the history list. Adjusted App to include an additional Counter instance for demonstration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
-import { log } from './log.js';
+import {log} from './log.js';
 import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
+    log('<App /> rendered');
 
-  const [chosenCount, setChosenCount] = useState(0);
+    const [chosenCount, setChosenCount] = useState(0);
 
-  function handleSetCount (newCount) {
-    setChosenCount(newCount);
-  }
+    function handleSetCount(newCount) {
+        setChosenCount(newCount);
+    }
 
-  return (
-    <>
-      <Header />
-      <main>
-        <ConfigureCounter onSet={handleSetCount} />
-        <Counter initialCount={chosenCount} />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <main>
+                <ConfigureCounter onSet={handleSetCount}/>
+                <Counter initialCount={chosenCount}/>
+                <Counter initialCount={0}/>
+            </main>
+        </>
+    );
 }
 
 export default App;
